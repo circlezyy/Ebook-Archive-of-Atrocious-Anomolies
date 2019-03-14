234b1a222dfd93ff26674a97fa68d56741e158c4
David Montes <david.montes@sjsu.edu>
2019-03-14 10:51:02 -0700

    added tapping cursor

---
+++ Assets/Scripts/HotSpot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotSpot : MonoBehaviour
+{
+    ParticleSystem ps;
+    Vector3 startPosition;
+    float radius = 0.8f;
+    float rate = 5;
+
+    private void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        startPosition = transform.position;
+        InvokeRepeating("Disable", 3, 3);
+    }
+
+    private void OnEnable()
+    {}
+
+    void Update()
+    {
+        transform.position = new Vector3(startPosition.x + radius * Mathf.Sin(Time.time * rate), startPosition.y + radius * Mathf.Cos(Time.time * rate), startPosition.z);
+    }
+
+    private void Disable()
+    {
+        ps.Play();
+        //Debug.Log("Play");
+        //gameObject.SetActive(false);
+    }
+}
