8c2411124f5e89becb9ced60525b16ac663ba401
David Montes <david.montes@sjsu.edu>
2019-03-20 12:18:35 -0700

    updated sprites for movement

---
+++ Assets/Scripts/Layer2Move.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Layer2Move : MonoBehaviour
+{
+    public Transform startTransform;
+    public Transform endTransform;
+    public float speed = 10;
+    private float maxDistance;
+
+    private void OnEnable()
+    {
+        transform.position = startTransform.position;
+        maxDistance = Vector3.Distance(startTransform.position, endTransform.position);
+        StartCoroutine(Move());
+    }
+
+    IEnumerator Move()
+    {
+        while(Vector3.Distance(transform.position, endTransform.position) > 0.1f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, endTransform.position, Time.deltaTime * speed);
+            yield return null;
+        }
+        transform.position = endTransform.position;
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+}
